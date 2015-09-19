55e2b294c7a2cfa4aaff34ba747caa325b37a70c
Mark S <markspeed_718@yahoo.com>
2015-09-19 14:56:30 -0400

    Added configs

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
 require '../vendor/autoload.php';
+require_once '../vendor/php-activerecord/php-activerecord/ActiveRecord.php';
+
+$configs = parse_ini_file('../config.ini');
+
+$cfg = ActiveRecord\Config::instance();
+     $cfg->set_model_directory('/var/www/html/social_monitization/models');
+     $cfg->set_connections(array(
+         'development' => "mysql://". $configs['mysql_user'] .":" .$configs['mysql_password']. "@ .$configs['mysql_host']. "/".$configs['mysql_database']"));
+     $cfg->set_default_connection('development');
+
 
 $app = new \Slim\Slim();
 
