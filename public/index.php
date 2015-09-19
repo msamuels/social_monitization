e0e375972335451fb4c10280f50a9f9d20ead082
Mark S <markspeed_718@yahoo.com>
2015-09-19 14:58:26 -0400

    Added configs

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once '../vendor/php-activerecord/php-activerecord/ActiveRecord.php';
 $configs = parse_ini_file('../config.ini');
 
 $cfg = ActiveRecord\Config::instance();
-     $cfg->set_model_directory('/var/www/html/social_monitization/models');
+     $cfg->set_model_directory($configs['model_dir']);
      $cfg->set_connections(array(
          'development' => "mysql://". $configs['mysql_user'] .":" .$configs['mysql_password']. "@ .$configs['mysql_host']. "/".$configs['mysql_database']"));
      $cfg->set_default_connection('development');
