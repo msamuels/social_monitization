7d7a45205eee1b76bcea74de3c59b2d779df902a
Mark S <markspeed_718@yahoo.com>
2016-05-28 14:28:23 -0400

    Change page title

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Social Monitization</title>
+    <title>shareitcamp</title>
     <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/override.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
