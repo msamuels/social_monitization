e4c7abb3122d4d4ead79db9c6a4e9ca06209e328
Mark S <markspeed_718@yahoo.com>
2016-06-19 14:59:24 -0400

    Update

--- templates/list-rewards.php
+++ templates/list-rewards.php
@@ -17,7 +17,7 @@
                 <li>
                     <p><img src="images/rewards/<?php echo $reward->image; ?>" height="100" width="100"/></p>
                     <p><?php echo $reward->reward_name ?></p>
-                    <p><?php echo $reward->point_value; ?></p>
+                    <p><?php echo $reward->point_value; ?> Pts</p>
                 </li>
                 <?php
             }
