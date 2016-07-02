08256cbe4ea4709866b439d1e2e7ae04231596b9
Mark S <markspeed_718@yahoo.com>
2016-07-02 14:19:05 -0400

    Link campaign name and image on homepage

--- templates/supporter-login.php
+++ templates/supporter-login.php
@@ -44,33 +44,74 @@
 
         <div class="row">
             <div class="col-md-3">
-                <p><?php echo $campaigns[0]->campaign_name; ?></p>
+                <p><a href="supporter/campaign/<?php echo $campaigns[0]->campaign_id; ?>">
+                        <?php echo $campaigns[0]->campaign_name; ?>
+                    </a>
+                </p>
                 <p><i> by <?php echo $campaigns[0]->getProducer()->org_name; ?></i></<p>
-                <p><img src="images/screenshots/<?php echo $campaigns[0]->screen_shot; ?>"/></p>
+
+                <p><a href="supporter/campaign/<?php echo $campaigns[0]->campaign_id; ?>">
+                    <img src="images/screenshots/<?php echo $campaigns[0]->screen_shot; ?>"/>
+                    </a>
+                </p>
+
                 <a href="supporter/campaign/<?php echo $campaigns[0]->campaign_id; ?>" target="_parent">
                     <button class="btn support-btns">Support</button>
                 </a>
             </div>
+
+
             <div class="col-md-3">
-                <p><?php echo $campaigns[1]->campaign_name; ?></p>
+                <p><a href="supporter/campaign/<?php echo $campaigns[1]->campaign_id; ?>">
+                        <?php echo $campaigns[1]->campaign_name; ?>
+                    </a>
+                </p>
+
                 <p><i> by <?php echo $campaigns[1]->getProducer()->org_name; ?></i></<p>
-                <p><img src="images/screenshots/<?php echo $campaigns[1]->screen_shot; ?>"/></p>
+
+                <p><a href="supporter/campaign/<?php echo $campaigns[1]->campaign_id; ?>">
+                        <img src="images/screenshots/<?php echo $campaigns[1]->screen_shot; ?>"/>
+                    </a>
+                </p>
+
                 <a href="supporter/campaign/<?php echo $campaigns[1]->campaign_id; ?>" target="_parent">
                     <button class="btn support-btns">Support</button>
                 </a>
             </div>
+
+
             <div class="col-md-3">
-                <p><?php echo $campaigns[2]->campaign_name; ?></p>
+                <p><a href="supporter/campaign/<?php echo $campaigns[2]->campaign_id; ?>">
+                        <?php echo $campaigns[2]->campaign_name; ?>
+                    </a>
+                </p>
+
                 <p><i> by <?php echo $campaigns[2]->getProducer()->org_name; ?></i></<p>
-                <p><img src="images/screenshots/<?php echo $campaigns[2]->screen_shot; ?>"/></p>
+
+                <p><a href="supporter/campaign/<?php echo $campaigns[2]->campaign_id; ?>">
+                        <img src="images/screenshots/<?php echo $campaigns[2]->screen_shot; ?>"/>
+                    </a>
+                </p>
+
                 <a href="supporter/campaign/<?php echo $campaigns[2]->campaign_id; ?>" target="_parent">
                     <button class="btn support-btns">Support</button>
                 </a>
             </div>
+
+
             <div class="col-md-3">
-                <p><?php echo $campaigns[3]->campaign_name; ?></p>
+                <p><a href="supporter/campaign/<?php echo $campaigns[3]->campaign_id; ?>">
+                        <?php echo $campaigns[3]->campaign_name; ?>
+                    </a>
+                </p>
+
                 <p><i> by <?php echo $campaigns[3]->getProducer()->org_name; ?></i></<p>
-                <p><img src="images/screenshots/<?php echo $campaigns[3]->screen_shot; ?>"/></p>
+
+                <p><a href="supporter/campaign/<?php echo $campaigns[3]->campaign_id; ?>">
+                        <img src="images/screenshots/<?php echo $campaigns[3]->screen_shot; ?>"/>
+                    </a>
+                </p>
+
                 <a href="supporter/campaign/<?php echo $campaigns[3]->campaign_id; ?>" target="_parent">
                     <button class="btn support-btns">Support</button>
                 </a>
