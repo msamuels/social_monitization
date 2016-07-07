66728a5c8e49632ea79aff7957575d333d723549
Mark Samuels <msamuels@theorchard.com>
2016-07-07 18:05:14 -0400

    Right align fields

--- templates/admin/create-reward.php
+++ templates/admin/create-reward.php
@@ -16,28 +16,28 @@
 
 <form action="/save-reward"  enctype="multipart/form-data" method="POST" class="form-horizontal" role="form">
     <div class="form-group">
-        <label class="control-label col-sm-4">Reward Name:</label>
+        <label class="control-label col-sm-4">Reward Name</label>
         <div class=" col-sm-8">
             <input type="text" class="form-control" name="reward_name" />
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-sm-4">Quanity:</label>
+        <label class="control-label col-sm-4">Quanity</label>
         <div class=" col-sm-8">
             <input type="text" class="form-control" name="quantity_remaining" />
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-sm-4">Expiration date:</label>
+        <label class="control-label col-sm-4">Expiration date</label>
         <div class=" col-sm-8">
             <input type="text" class="form-control datepicker" name="expiration date" placeholder="yyyy-mm-dd"/>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-sm-4">Details:</label>
+        <label class="control-label col-sm-4">Details</label>
         <div class=" col-sm-8">
            <textarea rows="4" cols="50" name="details" class="form-control" ></textarea>
         </div>
@@ -45,35 +45,35 @@
 
 
     <div class="form-group">
-        <label class="control-label col-sm-4">Image:</label>
+        <label class="control-label col-sm-4">Image</label>
         <div class=" col-sm-8">
             <input type="file" name="image" id="image" />
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-sm-4">URL:</label>
+        <label class="control-label col-sm-4">URL</label>
         <div class=" col-sm-8">
             <input type="text"   class="form-control" name="url" />
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-sm-4">Point Value:</label>
+        <label class="control-label col-sm-4">Point Value</label>
         <div class=" col-sm-8">
             <input type="text"   class="form-control" name="point_value" />
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-sm-4">How will you provide this to us:</label>
+        <label class="control-label col-sm-4">How will you provide this to us</label>
         <div class=" col-sm-8">
             <textarea rows="4" cols="50" name="details" class="form-control">   </textarea>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-sm-4">Campaign (optional):</label>
+        <label class="control-label col-sm-4">Campaign (optional)</label>
         <div class="col-sm-8">
             <select name="campaign">
                 <option selected>Select One:</option>
