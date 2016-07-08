3606c6c2f4f5110656cbbb731acfc7b945db45b3
Mark Samuels <msamuels@theorchard.com>
2016-07-08 12:46:56 -0400

    Update layout

--- templates/supported-campaigns.php
+++ templates/supported-campaigns.php
@@ -1,38 +1,54 @@
 <!-- @TODO remove margin-top -->
 <div class="row" id="supporters-list">
-    <div class="col-lg-12">
-<!-- Show campaigns supported -->
-<H1>Supported Campaigns</H1>
 
-<?php if(isset($success_info)){ ?>
-    <div class="alert alert-success"><?php echo $success_info; ?></div>
-<?php } ?>
-
-<ul class="list-things">
-    <?php
-    if (count($supported_campaigns) > 0) {
-        foreach ($supported_campaigns as $supported_campaign) {
-            ?>
-            <li>
-                <p><a href="/supporter/campaign/<?php echo $supported_campaign->campaign->campaign_id; ?>">
-                        <?php echo$supported_campaign->campaign->campaign_name; ?>
-                    </a>
-                </p>
-                <p><i> by <?php echo $supported_campaign->campaign->getProducer()->org_name; ?></i></p>
-                <p>
-                    <a href="/supporter/campaign/<?php echo $supported_campaign->campaign->campaign_id; ?>" >
-                        <img src="/images/screenshots/<?php echo $supported_campaign->campaign->screen_shot; ?>" height="100" width="100"/>
-                    </a>
-                </p>
-                <p class="list-campaign-copy"><?php echo substr($supported_campaign->campaign->copy, 0, 50); ?>...</p>
-                <p><button class="btn btn-success" type="submit">Support Pledged</button></p>
-                <div class="fb-share-button" data-href="<?php echo $base_url; ?>/supporter/campaign/<?php echo $supported_campaign->campaign->campaign_id; ?>" data-layout="button_count" data-mobile-iframe="true"></div>
-            </li>
-            <?php
-        }
-    }
-    ?>
-
-</ul>
+    <div class="col-sm-3">
     </div>
+
+    <div class="col-sm-6">
+        <!-- Show campaigns supported -->
+        <H1>Supported Campaigns</H1>
+
+        <?php if (isset($success_info)) { ?>
+            <div class="alert alert-success"><?php echo $success_info; ?></div>
+        <?php } ?>
+
+        <ul class="list-things">
+            <?php
+            if (count($supported_campaigns) > 0) {
+                foreach ($supported_campaigns as $supported_campaign) {
+                    ?>
+                    <li>
+                        <p><a href="/supporter/campaign/<?php echo $supported_campaign->campaign->campaign_id; ?>">
+                                <?php echo $supported_campaign->campaign->campaign_name; ?>
+                            </a>
+                        </p>
+                        <p><i> by <?php echo $supported_campaign->campaign->getProducer()->org_name; ?></i></p>
+                        <p>
+                            <a href="/supporter/campaign/<?php echo $supported_campaign->campaign->campaign_id; ?>">
+                                <img src="/images/screenshots/<?php echo $supported_campaign->campaign->screen_shot; ?>"
+                                     height="100" width="100"/>
+                            </a>
+                        </p>
+                        <p class="list-campaign-copy"><?php echo substr($supported_campaign->campaign->copy, 0, 50); ?>
+                            ...</p>
+                        <p>
+                            <button class="btn btn-success" type="submit">Support Pledged</button>
+                        </p>
+                        <div class="fb-share-button"
+                             data-href="<?php echo $base_url; ?>/supporter/campaign/<?php echo $supported_campaign->campaign->campaign_id; ?>"
+                             data-layout="button_count" data-mobile-iframe="true"></div>
+                    </li>
+                    <?php
+                }
+            }
+            ?>
+
+        </ul>
+
+    </div>
+
+    <div class="col-sm-3">
+    </div>
+
+
 </div>
