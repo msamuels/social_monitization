8b8126d40bb5aba9ff9452ffc338399b4af9d2d2
Mark Samuels <msamuels@theorchard.com>
2016-07-08 11:24:08 -0400

    Link fix

--- templates/header.php
+++ templates/header.php
@@ -55,7 +55,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/"><img src="images/iliv8-Logo2-PNG.png"/></a>
+            <a class="navbar-brand" href="/"><img src="/images/iliv8-Logo2-PNG.png"/></a>
         </div>
         <div class="navbar-default" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
