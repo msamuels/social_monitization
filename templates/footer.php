b5f7070e64448081d78856f779b1ee22ba8badad
Mark S <markspeed_718@yahoo.com>
2016-07-10 14:13:24 -0400

    Twitter follow button to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,21 +1,24 @@
- 
 <div class="row" id="bottom-nav">
-	<div class="col-xs-12">
-		<nav class="navbar-default" id="myNavbar">
-			<ul class="nav navbar-nav footer-nav">
-				<li><a class="footer-nav" href="/">get started</a></li>
-				<li><a class="footer-nav" href="/about-us">about Us</a></li>
-				<li><a class="footer-nav" href="/faqs">faq</a></li>
-				<li><a class="footer-nav" href="/">avertisements</a></li>
-				<li><a class="footer-nav" href="/">blog</a></li>
-			</ul>
-		</nav>
-	</div>
+    <div class="col-xs-12"">
+        <nav class="navbar-default" id="myNavbar">
+            <ul class="nav navbar-nav footer-nav">
+                <li><a class="footer-nav" href="/">get started</a></li>
+                <li><a class="footer-nav" href="/about-us">about Us</a></li>
+                <li><a class="footer-nav" href="/faqs">faq</a></li>
+                <li><a class="footer-nav" href="/">avertisements</a></li>
+                <li><a class="footer-nav" href="/">blog</a></li>
+                <li style="padding-top: 15px"><a href="https://twitter.com/shareitcamp" class="twitter-follow-button" data-show-count="false">Follow
+                        @shareitcamp</a>
+                    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </div>
 
 <div class="row">
-	Copyright 2016 shareitcamp.com. All rights reserved<br />
-	Terms of Service | Privacy Policy
+    Copyright 2016 shareitcamp.com. All rights reserved<br/>
+    Terms of Service | Privacy Policy
 </div>
 
 </div>
