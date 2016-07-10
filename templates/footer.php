5b51abd26395ad7867305984a3aa7424ca0dd2f7
Mark S <markspeed_718@yahoo.com>
2016-07-10 14:24:14 -0400

    Facebook follow icon

--- templates/footer.php
+++ templates/footer.php
@@ -1,19 +1,25 @@
 <div class="row" id="bottom-nav">
-    <div class="col-xs-12"">
-        <nav class="navbar-default" id="myNavbar">
-            <ul class="nav navbar-nav footer-nav">
-                <li><a class="footer-nav" href="/">get started</a></li>
-                <li><a class="footer-nav" href="/about-us">about Us</a></li>
-                <li><a class="footer-nav" href="/faqs">faq</a></li>
-                <li><a class="footer-nav" href="/">avertisements</a></li>
-                <li><a class="footer-nav" href="/">blog</a></li>
-                <li style="padding-top: 15px"><a href="https://twitter.com/shareitcamp" class="twitter-follow-button" data-show-count="false">Follow
-                        @shareitcamp</a>
-                    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-                </li>
-            </ul>
-        </nav>
-    </div>
+    <div class="col-xs-12"
+    ">
+    <nav class="navbar-default" id="myNavbar">
+        <ul class="nav navbar-nav footer-nav">
+            <li><a class="footer-nav" href="/">get started</a></li>
+            <li><a class="footer-nav" href="/about-us">about Us</a></li>
+            <li><a class="footer-nav" href="/faqs">faq</a></li>
+            <li><a class="footer-nav" href="/">avertisements</a></li>
+            <li><a class="footer-nav" href="/">blog</a></li>
+            <li style="padding-top: 15px"><a href="https://twitter.com/shareitcamp" class="twitter-follow-button"
+                                             data-show-count="false">Follow
+                    @shareitcamp</a>
+                <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </li>
+            <li style="padding-top: 9px; padding-left: 25px">
+                <div class="fb-like" data-href="https://www.facebook.com/shareitcamp" data-layout="standard"
+                     data-action="like" data-size="small" data-show-faces="false" data-share="true"></div>
+            </li>
+        </ul>
+    </nav>
+</div>
 </div>
 
 <div class="row">
