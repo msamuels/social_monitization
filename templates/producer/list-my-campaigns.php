91b453284f8b59a63894cbb039ae2d7fd4ac8956
Mark Samuels <msamuels@theorchard.com>
2016-07-14 15:13:18 -0400

    Image size update

--- templates/producer/list-my-campaigns.php
+++ templates/producer/list-my-campaigns.php
@@ -21,7 +21,7 @@
                     <p>
                     <p>
                         <a href="/producer/campaign/<?php echo $campaign->campaign_id; ?>">
-                            <img src="/images/screenshots/<?php echo $campaign->screen_shot; ?>" height="200"/>
+                            <img src="/images/screenshots/<?php echo $campaign->screen_shot; ?>" height="200" width="200"/>
                         </a>
                     </p>
                     <p class="list-campaign-copy"><?php echo substr($campaign->copy, 0, 50); ?>
