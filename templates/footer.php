9abc44718f24fc1eb98331245bcf541329b2b979
Mark Samuels <msamuels@theorchard.com>
2016-07-19 16:05:22 -0400

    Add proper facebook and twitter logos

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,5 @@
 <div class="row" id="bottom-nav">
-    <div class="col-xs-12"
-    ">
+    <div class="col-xs-12">
     <nav class="navbar-default" id="myNavbar">
         <ul class="nav navbar-nav footer-nav">
             <li><a class="footer-nav" href="/">get started</a></li>
@@ -8,25 +7,27 @@
             <li><a class="footer-nav" href="/faqs">faq</a></li>
             <li><a class="footer-nav" href="/">avertisements</a></li>
             <li><a class="footer-nav" href="/">blog</a></li>
-            <li style="padding-top: 15px"><a href="https://twitter.com/shareitcamp" class="twitter-follow-button"
-                                             data-show-count="false">Follow
-                    @shareitcamp</a>
-                <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </li>
-            <li style="padding-top: 9px; padding-left: 25px">
-                <div class="fb-like" data-href="https://www.facebook.com/shareitcamp" data-layout="standard"
-                     data-action="like" data-size="small" data-show-faces="false" data-share="true"></div>
-            </li>
         </ul>
     </nav>
 </div>
 </div>
 
-<div class="row" style="font-size: small; color: #8c8c8c;">
-    Copyright 2016 shareitcamp.com. All rights reserved<br/>
-    Terms of Service | Privacy Policy
-</div>
+<div class="row" style="font-size: small; color: #8c8c8c; margin-top: 5px;">
 
+    <div class="col-sm-11">
+        Copyright 2016 shareitcamp.com. All rights reserved<br/>
+        Terms of Service | Privacy Policy
+    </div>
+
+    <div class="col-sm-1" style="text-align: right">
+        <a href="https://twitter.com/shareitcamp" title="Twitter (Shareitcamp)" target="_blank">
+            <img alt="Twitter (Shareitcamp)" src="/images/twitter.png" class="icon-2"></a>
+            &nbsp;&nbsp;
+        <a href="https://www.facebook.com/shareitcamp" title="Facebook (Shareitcamp)" target="_blank">
+            <img alt="Facebook (Shareitcamp)" src="/images/facebook.png" class="icon-2"></a>
+    </div>
+
+</div>
 </div>
 
 
