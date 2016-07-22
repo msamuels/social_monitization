6358ff84fce2bb743c8ec804db671013248c9f82
Mark S <markspeed_718@yahoo.com>
2016-07-21 22:54:19 -0400

    About us update

--- templates/frontpage/about.php
+++ templates/frontpage/about.php
@@ -10,39 +10,38 @@
         <H2>SHARING IS CARING</H2>
 
         <P>Every day we share things with our friends and family on social media: what we are up to; what's happening
-            around us; what made us laugh.
+		around us; what made us laugh.
         </P> 
 
         <P>Occasionally, we will come across a person or a group that is doing something really impactful and innovative
-            and we'll share that too. Doing so helps them tremendously because it brings much needed attention to their
-            work. That's great.
-            Well we want to make this as common and easy as sharing a top 10 list.</P>
+		and we'll share that too. Doing so helps them tremendously because it brings much needed attention to their
+		work. That's great! Well at shareitcamp we want to make this as common and easy as sharing a top 10 list. </P>
          
-        <H2>SHAREITCAMP MAKES IT SIMPLE</H2>
+        <H2>SHAREITCAMP MAKES IT SIMPLE...</H2>
 
-        <p>To do this we have developed this platform where the producers of these efforts can present what they are
-            working on and where you can help them get exposure by simply sharing this on social media with your friends
-            and family. These efforts range from large undertakings (e.g. a non-profit helping to build schools across
-            Jamaica) to the small (e.g. local entrepreneurs creating a breakthrough app). Regardless of the size, what
-            we are asking you to do remains the same: share it with you network.
+        <p>To do this we have developed this platform where the producers of these efforts can present what they are 
+		working on and you can help them get exposure by simply sharing this with your friends and family. 
+		These efforts range from large undertakings (e.g. a non-profit helping to build schools across Jamaica) 
+		to the small (e.g. local entrepreneurs creating a breakthrough app). Regardless of the size, what we 
+		are asking you to do remains the same: share it with your network.
         </p> 
 
-        <h2>AND MAKES IT REWARDING</h2>
+        <h2>...AND MAKES IT REWARDING</h2>
 
-        <P>We think that this act of helping to raise awareness of work that is valuable is not something small. You are
-            not only helping by directing attention their way but ultimately you are helping them in their mission of
-            making a positive impact.
+        <P>We think that this act of helping to raise awareness of work that is valuable is a special act. You are 
+		not only helping by directing attention their way but ultimately you are helping them in their mission 
+		of making a positive impact. 
         </P> 
 
-        <P>While the feeling you get is rewarding in itself we want to go a step further and personally (can we say
-            personally as a company?) reward you. That is why for every 'campaign' you support we will credit you with
-            reward points. These points can be exchanged for things like gift cards.
+        <P>While the feeling you get is rewarding in itself we want to go a step further and personally (can we say 
+		personally as a company?) reward you.  That is why for every 'campaign' you support we will credit 
+		you with reward points. These points can be exchanged for some cool things like gift cards. 
         </P> 
 
         <h2>SHORT VERSION</h2>
 
-        <p>We could have probably summed up all of that by saying “shareitcamp is a platform that aims to make sharing
-            even more rewarding.”</p>
+        <p>We could have probably summed up all of that by saying shareitcamp is a platform that aims to make sharing 
+		even more rewarding.</p>
     </div>
 
 </div>
