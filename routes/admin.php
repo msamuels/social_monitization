e3d1fc052064e67261058a9c58da7e8a831c1f26
Mark S <markspeed_718@yahoo.com>
2016-08-27 11:36:53 -0400

    temp remove extension check rewards

--- routes/admin.php
+++ routes/admin.php
@@ -28,11 +28,11 @@ $app->post('/save-reward', $authenticate($app), function () use ($app){
     // ensure only allowed filetypes make it in
     $allowed =  array('png' ,'jpg');
     $filename = $upload->uploadFieldName;
-    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+    /*$ext = pathinfo($filename, PATHINFO_EXTENSION);
     if(!in_array($ext,$allowed) ) {
         $app->flash('success_info', 'Error: Invalid file type');
         $app->redirect('/create-reward');
-    }
+    }*/
 
     $rename_to = strtotime("now") .".jpg";
 
