501af06737abd8d4217c53ccd5b9185a2cc5ddf2
Mark Samuels <msamuels@theorchard.com>
2016-08-29 16:49:12 -0400

    Remove hard-coded url from  fb header page

--- public/hooks.php
+++ public/hooks.php
@@ -4,10 +4,13 @@ $app->hook('slim.before.dispatch', function () use ($app) {
 
     $parts = explode('/', $app->request->getPath());
 
+    $base_url = $app->config('configs')['base_url'];
+
     if($parts[1] == "supporter" && $parts[2] == "campaign"){
 
         $campaign = Campaign::find_by_campaign_id($parts[3]);
-        $app->render('../templates/facebook_header.php', array('campaign'=>$campaign, 'path' => $parts));
+        $app->render('../templates/facebook_header.php', array('campaign'=>$campaign, 'path' => $parts,
+            'base_url' => $base_url));
 
     }else {
 	    $app->render('../templates/header.php', array('path' => $parts));
