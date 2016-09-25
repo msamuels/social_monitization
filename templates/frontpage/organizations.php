043825c47f141455458c3b6e692fd4b9a7218bc2
Mark S <markspeed_718@yahoo.com>
2016-09-25 18:53:35 -0400

    Organizations line up

--- templates/frontpage/organizations.php
+++ templates/frontpage/organizations.php
@@ -1,98 +1,146 @@
-<style>
+<!-- HOME -->
+<section class="home bg-img-2" id="home">
+    <div class="bg-overlay"></div>
+    <!-- <div class="bg-overlay"></div> -->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
 
-	#get-started-banner{
-		background-image: none;
-	}
-</style>
-<div class="row" id="get-started-banner">
-    <H2 style="color: white" id="call-out">Partner with your supporters to <br />Promote your Campaigns</H2>
-    <p><a href="/create-producer" class="btn support-btns">Get Started</a></p>
-</div>
+                <div class="home-wrapper text-center">
+                    <h2 class="animated fadeInDown wow text-white" data-wow-delay=".1s">
+                        Partner with your supporters to <br/>Promote your Campaigns
+                    </h2>
+                    <a href="/create-producer" class="btn btn-primary btn-shadow btn-rounded w-lg animated fadeInDown wow"
+                       data-wow-delay=".4s">Get Started</a>
+                    <div class="clearfix"></div>
+                </div><!-- home wrapper -->
 
-<div class="row">
-	<div class="col-xs-12">
-		<strong>Why use Shareitcamp?</strong>
-	</div>
-</div>
-
-
-
-<div class="row">
-    <div class="col-xs-4" class="how-it-works-steps" style="text-align:right"><img class="icon" src="images/hourglass.png" /></div>
-    <div class="col-xs-8 col-sm-4 pull-left" class="how-it-works-steps">
-	<H3>Efficient social outreach</H3>
-        <p>Spend your time on more important things. We help to bring your supporters to your message.</p>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-xs-4" class="how-it-works-steps" style="text-align:right"><img class="icon" src="images/handsup.png" /></div>
-    <div class="col-xs-8 col-sm-4 pull-left" class="how-it-works-steps">
-	<H3>Engage your supporters</H3>
-        <p>Your supporters have been great for your brand. Here you can bring them on board as part of your
-           promotion team and reward them for it… </p>
-    </div>
-</div>
+            </div> <!-- end col -->
+        </div> <!-- end row -->
+    </div> <!-- end container -->
+</section>
 
 
-<div class="row">
-    <div class="col-xs-4" class="how-it-works-steps" style="text-align:right"><img class="icon" src="images/reward.png" /></div>
-    <div class="col-xs-8 col-sm-4 pull-left" class="how-it-works-steps">
-	<H3>We reward your supporters</H3>
-        <p>Your supporters earn points for each campaign they support. These can be redeemed for 
-            rewards, on us. Feel free to contribute too though. </p>
-    </div>
-</div>
+<section class="section" id="features">
 
-<div class="row">
-	<div class="col-xs-12">
-		<a class="btn support-btns" href="create-campaign">Start a campaign today</a>
-	</div>
-</div>
+    <div class="container">
 
-<br /><br />
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="title-box">
+                    <h3 class="fadeIn animated wow" data-wow-delay=".1s">Why use Shareitcamp?</h3>
+                    <div class="border"></div>
+                </div>
+            </div>
+        </div> <!-- end row -->
 
-<div class="row">
-<div class="col-sm-4"></div>
-<div class="col-sm-4"><H1>How it works</H1></div>
-<div class="col-sm-4"></div>
-</div>
+        <div class="row">
+            <div class="col-xs-4" style="text-align:right"><img class="icon"
+                                                                src="images/hourglass.png"/>
+            </div>
+            <div class="col-xs-8 col-sm-4 pull-left">
+                <H3>Efficient social outreach</H3>
+                <p>Spend your time on more important things. We help to bring your supporters to your message.</p>
+            </div>
+        </div>
 
-<div class="row" id="how-it-works">
+        <div class="row">
+            <div class="col-xs-4" style="text-align:right"><img class="icon"
+                                                                src="images/handsup.png"/>
+            </div>
+            <div class="col-xs-8 col-sm-4 pull-left">
+                <H3>Engage your supporters</H3>
+                <p>Your supporters have been great for your brand. Here you can bring them on board as part of your
+                    promotion team and reward them for it… </p>
+            </div>
+        </div>
 
-	<div class="col-sm-1"></div>
 
-	<div class="col-sm-3">
-		<div><img class="icon" src="images/no1.png" /></div>
-		<div>
-			<p><H2>Complete short form</H2></p>
-			<p class="desc">Give us a few details about the campaign you are promoting,
+        <div class="row">
+            <div class="col-xs-4" style="text-align:right"><img class="icon"
+                                                                src="images/reward.png"/>
+            </div>
+            <div class="col-xs-8 col-sm-4 pull-left">
+                <H3>We reward your supporters</H3>
+                <p>Your supporters earn points for each campaign they support. These can be redeemed for
+                    rewards, on us. Feel free to contribute too though. </p>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="title-box">
+                    <a class="btn btn-primary btn-shadow btn-rounded w-lg animated fadeInDown wow"
+                       href="create-campaign">Start a campaign today</a>
+                </div>
+            </div>
+        </div> <!-- end row -->
+
+    </div> <!-- end container -->
+</section>
+<!-- END FEATURES -->
+
+
+<section class="section" id="features">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="title-box">
+                    <h3 class="fadeIn animated wow" data-wow-delay=".1s">How it works</h3>
+                    <div class="border"></div>
+                </div>
+            </div>
+        </div> <!-- end row -->
+
+
+        <div class="row text-center">
+            <div class="col-sm-4">
+                <div class="service-item animated fadeInLeft wow" data-wow-delay=".1s">
+                    <img src="images/no1.png" width="48" alt="img">
+                    <div class="service-detail">
+                        <h4>Complete short form<</h4>
+                        <p>Give us a few details about the campaign you are promoting,
                             preferably with a picture and a link.</p>
-		</div>
-	</div>
-
-	<div class="col-sm-3 ">
-		<div><img class="icon" src="images/no2.png" /></div>
-		<div>
-			<p><H2>Set a 'reply by date'</H2></p>
-			<p class="desc">Give supporters a deadline by which they need to support your campaign.</p>
-		</div>
-	</div>
+                    </div> <!-- /service-detail -->
+                </div> <!-- /service-item -->
+            </div> <!-- /col -->
 
 
-	<div class="col-sm-4">
-		<div><img class="icon" src="images/no3.png" /></div>
-		<div>
-			<p><H2>Supporters share your message</H2></p>
-			<p class="desc">Supporters agree to share your message with their social networks. </p>
-		</div>
-	</div>
+            <div class="row text-center">
+                <div class="col-sm-4">
+                    <div class="service-item animated fadeInLeft wow" data-wow-delay=".1s">
+                        <img src="images/no2.png" width="48" alt="img">
+                        <div class="service-detail">
+                            <h4>Set a 'reply by date'</h4>
+                            <p>Give supporters a deadline by which they need to support your campaign.</p>
+                        </div> <!-- /service-detail -->
+                    </div> <!-- /service-item -->
+                </div> <!-- /col -->
 
-	<div class="col-sm-1"></div>
-</div>
+                <div class="row text-center">
+                    <div class="col-sm-4">
+                        <div class="service-item animated fadeInLeft wow" data-wow-delay=".1s">
+                            <img src="images/no2.png" width="48" alt="img">
+                            <div class="service-detail">
+                                <h4>Supporters share your message<</h4>
+                                <p>Supporters agree to share your message with their social networks. </p>
+                            </div> <!-- /service-detail -->
+                        </div> <!-- /service-item -->
+                    </div> <!-- /col -->
 
-<div class="row">
-    <div class="col-xs-12">
-        <a class="btn support-btns" href="create-campaign">Get Started</a>
-    </div>
-</div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <a class="btn btn-primary btn-shadow btn-rounded w-lg animated fadeInDown wow"
+                               href="create-campaign">Get Started</a>
+                        </div>
+                    </div>
+
+
+                </div> <!-- end container -->
+
+</section>
+<!-- END FEATURES -->
