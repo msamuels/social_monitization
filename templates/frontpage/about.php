ed9f6ed84fb21919d090f0d1ac716e761639f69c
Mark Samuels <msamuels@theorchard.com>
2016-09-29 13:31:22 -0400

    Producer logged in cleanup

--- templates/frontpage/about.php
+++ templates/frontpage/about.php
@@ -1,50 +1,70 @@
-<div class="row">
+<section class="section" id="features">
+    <div class="container">
 
-<div class="col-sm-3"></div>
+        <div class="row">
 
-<div class="col-sm-6">
+            <div class="col-sm-3"></div>
 
-    <div class="row"><H1>ABOUT US</H1></div>
+            <div class="col-sm-6">
 
-    <div class="row">
-        <H2>SHARING IS CARING</H2>
+                <div class="row"><H1>ABOUT US</H1></div>
 
-        <P>Every day we share things with our friends and family on social media: what we are up to; what's happening
-		around us; what made us laugh.
-        </P> 
+                <div class="row">
+                    <H2>SHARING IS CARING</H2>
 
-        <P>Occasionally, we will come across a person or a group that is doing something really impactful and innovative
-		and we'll share that too. Doing so helps them tremendously because it brings much needed attention to their
-		work. That's great! Well at shareitcamp we want to make this as common and easy as sharing a top 10 list. </P>
-         
-        <H2>SHAREITCAMP MAKES IT SIMPLE...</H2>
+                    <P>Every day we share things with our friends and family on social media: what we are up to; what's
+                        happening
+                        around us; what made us laugh.
+                    </P> 
 
-        <p>To do this we have developed this platform where the producers of these efforts can present what they are 
-		working on and you can help them get exposure by simply sharing this with your friends and family. 
-		These efforts range from large undertakings (e.g. a non-profit helping to build schools across Jamaica) 
-		to the small (e.g. local entrepreneurs creating a breakthrough app). Regardless of the size, what we 
-		are asking you to do remains the same: share it with your network.
-        </p> 
+                    <P>Occasionally, we will come across a person or a group that is doing something really impactful
+                        and innovative
+                        and we'll share that too. Doing so helps them tremendously because it brings much needed
+                        attention to their
+                        work. That's great! Well at shareitcamp we want to make this as common and easy as sharing a top
+                        10 list. </P>
+                     
+                    <H2>SHAREITCAMP MAKES IT SIMPLE...</H2>
 
-        <h2>...AND MAKES IT REWARDING</h2>
+                    <p>To do this we have developed this platform where the producers of these efforts can present what
+                        they are
+                        working on and you can help them get exposure by simply sharing this with your friends and
+                        family.
+                        These efforts range from large undertakings (e.g. a non-profit helping to build schools across
+                        Jamaica)
+                        to the small (e.g. local entrepreneurs creating a breakthrough app). Regardless of the size,
+                        what we
+                        are asking you to do remains the same: share it with your network.
+                    </p> 
 
-        <P>We think that this act of helping to raise awareness of work that is valuable is a special act. You are 
-		not only helping by directing attention their way but ultimately you are helping them in their mission 
-		of making a positive impact. 
-        </P> 
+                    <h2>...AND MAKES IT REWARDING</h2>
 
-        <P>While the feeling you get is rewarding in itself we want to go a step further and personally (can we say 
-		personally as a company?) reward you.  That is why for every 'campaign' you support we will credit 
-		you with reward points. These points can be exchanged for some cool things like gift cards. 
-        </P> 
+                    <P>We think that this act of helping to raise awareness of work that is valuable is a special act.
+                        You are
+                        not only helping by directing attention their way but ultimately you are helping them in their
+                        mission
+                        of making a positive impact.
+                    </P> 
 
-        <h2>SHORT VERSION</h2>
+                    <P>While the feeling you get is rewarding in itself we want to go a step further and personally (can
+                        we say
+                        personally as a company?) reward you. That is why for every 'campaign' you support we will
+                        credit
+                        you with reward points. These points can be exchanged for some cool things like gift cards.
+                    </P> 
 
-        <p>We could have probably summed up all of that by saying shareitcamp is a platform that aims to make sharing 
-		even more rewarding.</p>
-    </div>
+                    <h2>SHORT VERSION</h2>
 
-</div>
-</div>
+                    <p>We could have probably summed up all of that by saying shareitcamp is a platform that aims to
+                        make sharing
+                        even more rewarding.</p>
+                </div>
 
-<div class="col-sm-3"></div>
+            </div>
+        </div>
+
+        <div class="col-sm-3"></div>
+
+
+    </div><!--end container -->
+</section><!--end section -->
--- templates/frontpage/faq.php
+++ templates/frontpage/faq.php
@@ -1,132 +1,148 @@
-<div class="row">
+<section class="section" id="features">
+    <div class="container">
 
-    <div class="col-sm-3"></div>
+        <div class="row">
 
-    <div class="col-sm-6">
-        <a href="#producer"> Producer</a> | <a href="#supporter" class = "highlighted"> Supporter</a> </p>
+            <div class="col-sm-3"></div>
 
-<a name="producer">Producer</a>
+            <div class="col-sm-6">
+                <a href="#producer"> Producer</a> | <a href="#supporter" class="highlighted"> Supporter</a> </p>
 
-            <p><strong>Can persons donate on shareitecamp.com?</strong></p>
+                <a name="producer">Producer</a>
 
-            <p>No, shareitcamp.com does not accept any monetary donations from supporters for the purpose of
-                fundraising.
-                Its purpose is to build a collective network of supporters to build awareness of such an effort,
-                increasing the
-                pool of potential donors.</p>
+                <p><strong>Can persons donate on shareitecamp.com?</strong></p>
 
-
-            <p><strong>What kind of campaign can I post?</strong></p>
-
-            <p>Post content promoting initiatives that your organization has coming up such as fundraisers, school
-                supply drives,
-                cultural events, scholarship opportunities, or crowdfunding initiatives. Have an innovation or offerings
-                that's more
-                product or service focused? Post that as well.</p>
+                <p>No, shareitcamp.com does not accept any monetary donations from supporters for the purpose of
+                    fundraising.
+                    Its purpose is to build a collective network of supporters to build awareness of such an effort,
+                    increasing the
+                    pool of potential donors.</p>
 
 
-            <p><strong>Who can post campaigns (i.e. be a producer) on shareitcamp?</strong></p>
+                <p><strong>What kind of campaign can I post?</strong></p>
 
-            <p>Shareitcamp is open to any person or organization with products, services, solutions or approaches that
-                meet the
-                needs of the Diaspora. Others should be put in a position to succeed through the success of their
-                offerings.</p>
-
-
-            <p><strong>Will my campaign be displayed exclusively to the supporters that I recruit?</strong></p>
-
-            <p>Your campaign will be shown to the total network of supporters on shareitcamp regardless of who recruits
-                them.</p>
-             
-
-            <p><strong>What social media platforms will my message appear on?</strong></p>
-
-            <p>Supporters can only post your messages to their Facebook via SIC at this time. We plan to expand this
-                soon.</p>
+                <p>Post content promoting initiatives that your organization has coming up such as fundraisers, school
+                    supply drives,
+                    cultural events, scholarship opportunities, or crowdfunding initiatives. Have an innovation or
+                    offerings
+                    that's more
+                    product or service focused? Post that as well.</p>
 
 
-            <p><strong> Who is responsible for providing the rewards?</strong></p>
+                <p><strong>Who can post campaigns (i.e. be a producer) on shareitcamp?</strong></p>
 
-            <p>shareitecamp is responsible for providing and fulfilling the rewards. We do encourage groups to donate
-                rewards if
-                they choose (their might be some cool incentives you come across that we aren't aware of). These rewards
-                can be
-                made available to the full network of supporters or only to supporters of your campaign.</p>
-
-
-            <p><strong> How frequently are supporters notified that new campaigns have been posted to
-                    shareitcamp.com?</strong></p>
-
-            <p>Supporters can choose how often they want to be notified of new campaigns.</p>
+                <p>Shareitcamp is open to any person or organization with products, services, solutions or approaches
+                    that
+                    meet the
+                    needs of the Diaspora. Others should be put in a position to succeed through the success of their
+                    offerings.</p>
 
 
-            <p><strong>How much is this going to cost me?</strong></p>
+                <p><strong>Will my campaign be displayed exclusively to the supporters that I recruit?</strong></p>
 
-            <p>shareitcamp is free to recognized non-profits, particularly Diaspora organizations who are pioneering
-                innovative
-                approaches that support and strengthen their communities.</p>
+                <p>Your campaign will be shown to the total network of supporters on shareitcamp regardless of who
+                    recruits
+                    them.</p>
+                 
 
+                <p><strong>What social media platforms will my message appear on?</strong></p>
 
-        <a name="supporter"> Supporter</a>
-            <p><strong>What is a campaign?</strong></p>
-
-            <p>On shareitcamp a campaign is a promotional message that a person or an organization posts to make others
-                aware of
-                their effort and take action. It could be a flyer for an event, a link to place to donate to a cause, an
-                article,
-                etc.</p>
-             
-            <p><strong>How will I know when a new campaign is posted?</strong></p>
-
-            <p>There are two ways: 1) we will send you an email about new campaigns, and 2) when you like us on Facebook
-                you'll
-                receive updates there as well.</p>
-
-            <p><strong>Why only Facebook?</strong></p>
-
-            <p>We are planning on expanding beyond Facebook but thought it was a good place to start.</p>
-
-            <p><strong>Is there a minimum number of Facebook friends that I need to be a supporter?</strong></p>
-
-            <p>Approximately 50 is preferred. This is more to make sure you are human and not a bot.</p>
-
-            <p><strong>How do I earn points?</strong></p>
-
-            <p>That's the easy part. You receive points when you support (share campaigns) on Facebook.</p>
-
-            <p><strong>What can these points be used for?</strong></p>
-
-            <p>Your points can be redeemed for 'rewards' such as gift cards. Once you are logged-in, just click on the
-                rewards
-                section to see what rewards are currently being offered.</p>
+                <p>Supporters can only post your messages to their Facebook via SIC at this time. We plan to expand this
+                    soon.</p>
 
 
-            <p><strong> Am I obligated to support the campaigns that are posted?</strong></p>
-            <p>Nope. Only support campaigns that you feel strongly about. On our part we will look to recruit
-                organizations that
-                are doing work that fits with our mission and values.</p>
+                <p><strong> Who is responsible for providing the rewards?</strong></p>
+
+                <p>shareitecamp is responsible for providing and fulfilling the rewards. We do encourage groups to
+                    donate
+                    rewards if
+                    they choose (their might be some cool incentives you come across that we aren't aware of). These
+                    rewards
+                    can be
+                    made available to the full network of supporters or only to supporters of your campaign.</p>
 
 
-            <p><strong>What happens when I click support?</strong></p>
-            <p>If you are logged in, the producer of the campaign is notified that you someone.</p>
+                <p><strong> How frequently are supporters notified that new campaigns have been posted to
+                        shareitcamp.com?</strong></p>
+
+                <p>Supporters can choose how often they want to be notified of new campaigns.</p>
 
 
-            <p><strong>How long do I have to post the content?</strong></p>
+                <p><strong>How much is this going to cost me?</strong></p>
 
-            On each campaign page you will see a 'reply by date', that is the deadline for hitting the support button.
-            You will then need to post the message to Facebook, by clicking the 'Facebook' icon on the campaign page within
-            the date range of the campaign period.</p>
-
-
-            <p><strong> Why do you ask for my Facebook follower count?</strong></p>
-            <p>We want to help the organizations understand how many people they are able to reach with their message.
-                We do not
-                share your name or any other identifying information about you.</p>
-
-    </div>
-
-    <div class="col-sm-3"></div>
-
-</div>
+                <p>shareitcamp is free to recognized non-profits, particularly Diaspora organizations who are pioneering
+                    innovative
+                    approaches that support and strengthen their communities.</p>
 
 
+                <a name="supporter"> Supporter</a>
+                <p><strong>What is a campaign?</strong></p>
+
+                <p>On shareitcamp a campaign is a promotional message that a person or an organization posts to make
+                    others
+                    aware of
+                    their effort and take action. It could be a flyer for an event, a link to place to donate to a
+                    cause, an
+                    article,
+                    etc.</p>
+                 
+                <p><strong>How will I know when a new campaign is posted?</strong></p>
+
+                <p>There are two ways: 1) we will send you an email about new campaigns, and 2) when you like us on
+                    Facebook
+                    you'll
+                    receive updates there as well.</p>
+
+                <p><strong>Why only Facebook?</strong></p>
+
+                <p>We are planning on expanding beyond Facebook but thought it was a good place to start.</p>
+
+                <p><strong>Is there a minimum number of Facebook friends that I need to be a supporter?</strong></p>
+
+                <p>Approximately 50 is preferred. This is more to make sure you are human and not a bot.</p>
+
+                <p><strong>How do I earn points?</strong></p>
+
+                <p>That's the easy part. You receive points when you support (share campaigns) on Facebook.</p>
+
+                <p><strong>What can these points be used for?</strong></p>
+
+                <p>Your points can be redeemed for 'rewards' such as gift cards. Once you are logged-in, just click on
+                    the
+                    rewards
+                    section to see what rewards are currently being offered.</p>
+
+
+                <p><strong> Am I obligated to support the campaigns that are posted?</strong></p>
+                <p>Nope. Only support campaigns that you feel strongly about. On our part we will look to recruit
+                    organizations that
+                    are doing work that fits with our mission and values.</p>
+
+
+                <p><strong>What happens when I click support?</strong></p>
+                <p>If you are logged in, the producer of the campaign is notified that you someone.</p>
+
+
+                <p><strong>How long do I have to post the content?</strong></p>
+
+                On each campaign page you will see a 'reply by date', that is the deadline for hitting the support
+                button.
+                You will then need to post the message to Facebook, by clicking the 'Facebook' icon on the campaign page
+                within
+                the date range of the campaign period.</p>
+
+
+                <p><strong> Why do you ask for my Facebook follower count?</strong></p>
+                <p>We want to help the organizations understand how many people they are able to reach with their
+                    message.
+                    We do not
+                    share your name or any other identifying information about you.</p>
+
+            </div>
+
+            <div class="col-sm-3"></div>
+
+        </div>
+
+    </div><!--end container -->
+</section><!--end section -->
