abd8ba083896c40dcd0504b76249b7a66863a358
Mark Samuels <msamuels@theorchard.com>
2016-09-29 13:39:25 -0400

    Supporte logged in cleanup

--- templates/list-rewards.php
+++ templates/list-rewards.php
@@ -1,32 +1,39 @@
-<div class="row">
+<section class="section" id="features">
+    <div class="container">
 
-    <div class="col-sm-3"></div>
+        <div class="row">
 
-    <div class="col-sm-6">
+            <div class="col-sm-3"></div>
 
-        My Points:
-        <H1><?php echo $points_earned; ?></H1>
+            <div class="col-sm-6">
 
-        <H1>Rewards</H1>
+                My Points:
+                <H1><?php echo $points_earned; ?></H1>
 
-        <ul class="list-things">
-            <?php
+                <H1>Rewards</H1>
 
-            foreach ($rewards as $reward) {
-                ?>
-                <li>
-                    <p><img src="images/rewards/<?php echo $reward->image; ?>" height="100" width="100"/></p>
-                    <p><?php echo $reward->reward_name ?></p>
-                    <p><?php echo $reward->point_value; ?> Pts</p>
-                </li>
-                <?php
-            }
-            ?>
+                <ul class="list-things">
+                    <?php
 
-        </ul>
+                    foreach ($rewards as $reward) {
+                        ?>
+                        <li>
+                            <p><img src="images/rewards/<?php echo $reward->image; ?>" height="100" width="100"/></p>
+                            <p><?php echo $reward->reward_name ?></p>
+                            <p><?php echo $reward->point_value; ?> Pts</p>
+                        </li>
+                        <?php
+                    }
+                    ?>
+
+                </ul>
 
 
-    </div>
-</div>
+            </div>
+        </div>
 
-<div class="col-sm-3"></div>
+        <div class="col-sm-3"></div>
+
+    </div> <!-- end container -->
+</section>
+<!-- END HOME -->
--- templates/supported-campaigns.php
+++ templates/supported-campaigns.php
@@ -1,52 +1,61 @@
-<!-- @TODO remove margin-top -->
-<div class="row" id="supporters-list">
+<section class="section" id="features">
+    <div class="container">
 
-    <div class="col-sm-2"></div>
+        <div class="row" id="supporters-list">
 
-    <div class="col-sm-8">
-        <!-- Show campaigns supported -->
-        <H1>Supported Campaigns</H1>
+            <div class="col-sm-2"></div>
 
-        <?php if (isset($success_info)) { ?>
-            <div class="alert alert-success"><?php echo $success_info; ?></div>
-        <?php } ?>
+            <div class="col-sm-8">
+                <!-- Show campaigns supported -->
+                <H1>Supported Campaigns</H1>
 
-        <ul class="list-things">
-            <?php
-            if (count($supported_campaigns) > 0) {
-                foreach ($supported_campaigns as $supported_campaign) {
-                    ?>
-                    <li class="list-item">
-                        <p><a href="/supporter/campaign/<?php echo $supported_campaign->campaign->friendly_url; ?>">
-                                <?php echo $supported_campaign->campaign->campaign_name; ?>
-                            </a>
-                        </p>
-                        <p class="by-line"><i> by <?php echo $supported_campaign->campaign->getProducer()->org_name; ?></i></p>
-                        <p>
-                            <a href="/supporter/campaign/<?php echo $supported_campaign->campaign->friendly_url; ?>">
-                                <img src="/images/screenshots/<?php echo $supported_campaign->campaign->screen_shot; ?>"
-                                     height="100" width="100"/>
-                            </a>
-                        </p>
-                        <p class="list-campaign-copy"><?php echo substr($supported_campaign->campaign->copy, 0, 50); ?>
-                            ...</p>
-                        <p>
-                            <button class="btn btn-success" type="submit">Support Pledged</button>
-                        </p>
-                        <div class="fb-share-button"
-                             data-href="<?php echo $base_url; ?>/supporter/campaign/<?php echo $supported_campaign->campaign->friendly_url; ?>"
-                             data-layout="button_count" data-mobile-iframe="true"></div>
-                    </li>
+                <?php if (isset($success_info)) { ?>
+                    <div class="alert alert-success"><?php echo $success_info; ?></div>
+                <?php } ?>
+
+                <ul class="list-things">
                     <?php
-                }
-            }
-            ?>
+                    if (count($supported_campaigns) > 0) {
+                        foreach ($supported_campaigns as $supported_campaign) {
+                            ?>
+                            <li class="list-item">
+                                <p>
+                                    <a href="/supporter/campaign/<?php echo $supported_campaign->campaign->friendly_url; ?>">
+                                        <?php echo $supported_campaign->campaign->campaign_name; ?>
+                                    </a>
+                                </p>
+                                <p class="by-line"><i>
+                                        by <?php echo $supported_campaign->campaign->getProducer()->org_name; ?></i></p>
+                                <p>
+                                    <a href="/supporter/campaign/<?php echo $supported_campaign->campaign->friendly_url; ?>">
+                                        <img
+                                            src="/images/screenshots/<?php echo $supported_campaign->campaign->screen_shot; ?>"
+                                            height="100" width="100"/>
+                                    </a>
+                                </p>
+                                <p class="list-campaign-copy"><?php echo substr($supported_campaign->campaign->copy, 0, 50); ?>
+                                    ...</p>
+                                <p>
+                                    <button class="btn btn-success" type="submit">Support Pledged</button>
+                                </p>
+                                <div class="fb-share-button"
+                                     data-href="<?php echo $base_url; ?>/supporter/campaign/<?php echo $supported_campaign->campaign->friendly_url; ?>"
+                                     data-layout="button_count" data-mobile-iframe="true"></div>
+                            </li>
+                            <?php
+                        }
+                    }
+                    ?>
 
-        </ul>
+                </ul>
 
-    </div>
+            </div>
 
-    <div class="col-sm-2"></div>
+            <div class="col-sm-2"></div>
 
 
-</div>
+        </div>
+
+    </div> <!-- end container -->
+</section>
+<!-- END HOME -->
