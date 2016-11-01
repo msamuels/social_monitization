81567dcaa7600714a9d01864d976cc59da2901fe
Mark S <markspeed_718@yahoo.com>
2016-11-01 00:23:52 -0400

    Facebook login

--- routes/auth.php
+++ routes/auth.php
@@ -114,7 +114,7 @@ $app->get("/login", function () use ($app) {
     $helper = $fb->getRedirectLoginHelper();
 
     $permissions = ['email']; // Optional permissions
-    $loginUrl = $helper->getLoginUrl($configs['app_url'].'/fb-callback.php', $permissions);
+    $loginUrl = $helper->getLoginUrl($configs['app_url'].'/fb-callback', $permissions);
 
    $app->render('login.php', array('error' => $error, 'username_value' => $username_value,
       'username_error' => $username_error, 'password_error' => $password_error, 'urlRedirect' => $urlRedirect,
@@ -187,3 +187,105 @@ function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzAB
     }
     return $str;
 }
+
+// forgot password
+$app->get("/fb-callback", function () use ($app) {
+
+
+    $configs = $app->config('configs');
+
+    $fb = new Facebook\Facebook([
+        'app_id' => $configs['fb_app_id'], // Replace {app-id} with your app id
+        'app_secret' =>  $configs['fb_app_secret'],
+        'default_graph_version' => 'v2.2',
+    ]);
+
+    $helper = $fb->getRedirectLoginHelper();
+
+    try {
+        $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+
+    if (! isset($accessToken)) {
+        if ($helper->getError()) {
+            header('HTTP/1.0 401 Unauthorized');
+            echo "Error: " . $helper->getError() . "\n";
+            echo "Error Code: " . $helper->getErrorCode() . "\n";
+            echo "Error Reason: " . $helper->getErrorReason() . "\n";
+            echo "Error Description: " . $helper->getErrorDescription() . "\n";
+        } else {
+            header('HTTP/1.0 400 Bad Request');
+            echo 'Bad request';
+        }
+        exit;
+    }
+
+// Logged in
+    echo '<h3>Access Token</h3>';
+    var_dump($accessToken->getValue());
+
+// The OAuth 2.0 client handler helps us manage access tokens
+    $oAuth2Client = $fb->getOAuth2Client();
+
+// Get the access token metadata from /debug_token
+    $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+    echo '<h3>Metadata</h3>';
+    var_dump($tokenMetadata);
+
+// Validation (these will throw FacebookSDKException's when they fail)
+    $tokenMetadata->validateAppId($configs['fb_app_id']); // Replace {app-id} with your app id
+// If you know the user ID this access token belongs to, you can validate it here
+//$tokenMetadata->validateUserId('123');
+    $tokenMetadata->validateExpiration();
+
+    if (! $accessToken->isLongLived()) {
+        // Exchanges a short-lived access token for a long-lived one
+        try {
+            $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+            exit;
+        }
+
+        echo '<h3>Long-lived</h3>';
+        var_dump($accessToken->getValue());
+    }
+
+    $_SESSION['fb_access_token'] = (string) $accessToken;
+
+// try to get logged in user info
+    try {
+        // Returns a `Facebook\FacebookResponse` object
+        $response = $fb->get('/me?fields=id,name', '{access-token}');
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+
+    $user = $response->getGraphUser();
+
+    var_dump($user);exit;
+
+    echo 'Name: ' . $user['name'];
+
+    $_SESSION['email'] = $user['email'];
+    $_SESSION['user'] = $user['name'];
+    $_SESSION['user_type'] = 'supporter';
+    exit;
+// User is logged in with a long-lived access token.
+// You can redirect them to a members-only page.
+    header('Location: '.$configs['base_url'].'/supporter/campaigns/pending');
+
+
+});
