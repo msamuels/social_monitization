6f840d859349140fef7f6fe5dbc96c9df74ed8e2
Mark Samuels <msamuels@theorchard.com>
2017-02-08 11:05:42 -0500

    Add total shares to invoice

--- templates/producer/invoice.php
+++ templates/producer/invoice.php
@@ -5,30 +5,6 @@
 
         <H3><?php echo $campaign->campaign_name; ?></H3>
 
-        <div class="row sharepoints">
-            <div class="col-sm-6">
-                <p class="sharepoints-p"> 1. Share This</p>
-                <div class="row">
-                    <div class="col-sm-3 sharebuttongap">
-                        <span class="fb-share-button" style="margin-bottom: 5px;"
-                              data-href="<?php echo $base_url; ?>/supporter/campaign/<?php echo $campaign->friendly_url; ?>"
-                              data-layout="button_count" data-mobile-iframe="true">
-                        </span>
-                    </div>
-
-                    <div class="col-sm-3 sharebuttongap">
-                        <span><a href="https://twitter.com/share" class="twitter-share-button"
-                                 data-via="shareitcamp"
-                                 data-show-count="false">Tweet</a>
-                        <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-                        </span>
-                    </div>
-
-                </div>
-
-            </div>
-
-
 
         <p><img width="300" height="500" style="max-height: 450px;" src="/images/screenshots/<?php echo $campaign->screen_shot; ?>"/></p>
 
@@ -59,6 +35,33 @@
             <label>Duration:</label> <?php echo date_format($campaign->start_date, 'F d, Y '); ?> -
             <?php echo date_format($campaign->end_date, 'F d, Y '); ?>
 
+            <H2>Total Shares:</H2>
+
+            <div class="row sharepoints">
+                <div class="col-sm-6">
+                    <p class="sharepoints-p"> 1. Share This</p>
+                    <div class="row">
+                        <div class="col-sm-3 sharebuttongap">
+                        <span class="fb-share-button" style="margin-bottom: 5px;"
+                              data-href="<?php echo $base_url; ?>/supporter/campaign/<?php echo $campaign->friendly_url; ?>"
+                              data-layout="button_count" data-mobile-iframe="true">
+                        </span>
+                        </div>
+
+                        <div class="col-sm-3 sharebuttongap">
+                        <span><a href="https://twitter.com/share" class="twitter-share-button"
+                                 data-via="shareitcamp"
+                                 data-show-count="false">Tweet</a>
+                        <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                        </span>
+                        </div>
+
+                    </div>
+
+                </div>
+
+        </div>
+
             <H2>Impressions:</H2>
             <?php
 
@@ -70,7 +73,6 @@
                 }
             }
             ?>
-        </div>
         <label>Estimated:</label> <input type="text" name="estimated_impression" value="<?php echo $i; ?>"/>
         <!--<label>Actual:</label> <input type="text" name="actual_impression" value=""/>-->
 
