b56b3956775692506dcc39f29b53a3a5457e35c7
yaadworks <yaadstyle@yahoo.com>
2017-05-26 00:26:32 -0400

    removed sumome from site

--- templates/facebook_header.php
+++ templates/facebook_header.php
@@ -58,10 +58,7 @@
 
     <script type="text/javascript" src="/fancybox/jquery.fancybox.pack.js"></script>
 
-    <!-- Test Site code
-   <script src="//load.sumome.com/" data-sumo-site-id="903902e8ecca928b627578f56a2123411545d219f683743e0293c3f22d4dbfd5" async="async"></script>
-   -->
-    <script src="//load.sumome.com/" data-sumo-site-id="63d03119b0f57bd789d47afb992766ce5b861812ec577960e3ccde28c598c6fa" async="async"></script>
+    
 
 </head>
 
