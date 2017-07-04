b7e8212bf170d8fdd3298796ed5eafcf3c80e7f3
yaadworks <yaadstyle@yahoo.com>
2017-07-03 21:26:55 -0400

    edited email

--- routes/frontpage.php
+++ routes/frontpage.php
@@ -84,7 +84,7 @@ $app->get('/termsandconditions', function () use ($app){
     // Email RSVPer to let them know we received their RSVP
         $subject_supporter = "Your RSVP was received!";    
         
-        $body_supporter = "<p> We have received your RSVP for the Advocacy, Policy and Leadership Workshop. If you have any question please email JA55APL@shareitcamp.com.See you on July 27th.  </p><br />        
+        $body_supporter = "<p> We have received your RSVP for the Advocacy, Policy and Leadership Workshop. If you have any questions please email JA55APL@shareitcamp.com.See you on July 27th.  </p><br />        
             <p>    Help spread the word about this workshop. Here's how:"; 
     
              $body_supporter .= " 
@@ -112,10 +112,10 @@ $app->get('/termsandconditions', function () use ($app){
                 margin: 4px 2px;
                 cursor: pointer;
             '>  Become a Supporter </a><br />";  
-        $body_supporter .= "<p>Thanks in advance, <br /> ShareItCamp.com</p>";              
+        $body_supporter .= "<p>Thanks in advance, <br /> APL Team</p>";              
     $baseurl =  $destination = $app->config('configs')['base_url'];    
     mail($req['rsvpEmail'], $subject_supporter, $body_supporter, $headers);
 
-    $app->flash('success_info', 'Email sent');
-    $app->redirect('/producer/Jamaica-55-Advocacy-Policy-and-Leadership-Workshop'); 
+    $app->flash('success_info', 'Thank You. Please check your email for confirmation.');
+    $app->redirect('/producer/shareitcamp'); 
 });
