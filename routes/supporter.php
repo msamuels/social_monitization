67d523760dbe3d4b79878822a2eedac2e1ba7cd9
Mark S <markspeed_718@yahoo.com>
2017-07-26 22:32:16 -0400

    Remove try catch

--- routes/supporter.php
+++ routes/supporter.php
@@ -55,13 +55,9 @@ $app->post('/save-supporter', function () use ($app){
 
         // Save Facebook affiliation
         if($req['followers_fb'] != "") {
-try{
             $fb_handles = Supporter_handles::create(
                 array('supporter_id' => $supporter->id_supporter, 'social_media_id' => 1, 'handle' => $req['fb_handle'], 'follower_count' => $req['followers_fb']
                 ));
-}catch(Exception $e){
-echo $e->getMessage();exit;
-}exit;
         }
 
         // Save Twitter affiliation
