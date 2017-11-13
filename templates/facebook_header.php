f5c2e3fa46ad9bae9d4f2c2b18293f5f9ac8cff6
yaadworks <yaadstyle@yahoo.com>
2017-11-12 23:33:37 -0500

    added addthis code to fb header

--- templates/facebook_header.php
+++ templates/facebook_header.php
@@ -74,6 +74,8 @@
 </head>
 
 <body data-spy="scroll" data-target="#navbar-menu">
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a091963b75e1753"></script>
 
 <script>
     window.fbAsyncInit = function() {
